e6ff770b985d9e845c51a79619d51e71d9248530
Than <tan95685@gmail.com>
2024-03-19 15:33:05 +0700

    start Q2

---
+++ Q2.py
@@ -0,0 +1,44 @@
+from pwn import *
+from Crypto.Hash import SHA256
+ip = "localhost"
+port = 5000 
+
+io = remote(ip, port)
+
+data1 = io.recvline().decode("utf-8")
+
+io.sendline(str(1))
+data1 = io.recvline().decode("utf-8")
+print(data1)
+data1 = io.recvline().decode("utf-8")
+print(data1)
+data1 = io.recvline().decode("utf-8")
+print(data1)
+UUID = []
+Hash=[]
+ans=""
+for i in range(20):
+    data1 = io.recvline().decode("utf-8")
+    data1 = data1.split()[2]
+    UUID.append(data1)
+    h = SHA256.new()
+    h.update(data1.encode())
+  
+    print(h.hexdigest())
+    data1 = io.recvline().decode("utf-8")
+    data1 = data1.split()[2]
+    Hash.append(data1)
+    print(Hash[i])
+    if h.hexdigest() == Hash[i]:
+        ans += "Y"
+    else:
+        ans+="N"
+    print("___________")
+data1 = io.recvline().decode("utf-8")
+data1 = io.recvline().decode("utf-8")
+
+print(ans)
+io.sendline(ans)
+data1 = io.recvline().decode("utf-8")
+print(data1)
+io.close()
